0dd081ec455e2b90f106f15841b3b019b931ddfb
Sam Hartman <hartmans@debian.org>
2000-11-30 20:13:15 +0000

    Initial revision

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pam_krb5):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="pam_krb5" command="/usr/bin/pam_krb5"
---
+++ libpam-krb5/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the pam_krb5 package
+#
+0 4	* * *	root	pam_krb5_maintenance
