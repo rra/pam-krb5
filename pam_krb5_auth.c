0949f125bc7a7b195a0b5c36550d07942f42c72d
Russ Allbery <eagle@windlord>
2006-11-17 17:15:17 -0800

    Avoid a use of already-freed memory when debugging is enabled.

--- pam_krb5_auth.c
+++ pam_krb5_auth.c
@@ -336,6 +336,7 @@ pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
      */
     if (flags & PAM_DELETE_CRED) {
         pamret = pam_set_data(pamh, "ctx", NULL, NULL);
+        ctx = NULL;
         goto done;
     }
 
@@ -363,13 +364,11 @@ pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
     }
 
     /*
-     * pamret holds the status of pamk5_context_fetch from above, so indicates
-     * whether we were able to successfully find the context from the previous
-     * authentication.  If we weren't, we were probably run by OpenSSH with
-     * its broken PAM handling, so we're going to cobble up a new context for
-     * ourselves.
+     * If we weren't able to obtain a context, we were probably run by OpenSSH
+     * with its broken PAM handling, so we're going to cobble up a new context
+     * for ourselves.
      */
-    if (pamret != PAM_SUCCESS) {
+    if (ctx == NULL) {
         pamk5_debug(ctx, args, "no context found, creating one");
         pamret = create_session_context(args, pamh, &ctx);
         if (ctx == NULL)
